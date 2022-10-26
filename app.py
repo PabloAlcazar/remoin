bfc445f5ebe5a09b6e059447dc0b6fd73dbe2ad5
Alcázar Martín-Borja <pablo.martin-borja@accenture.com>
2022-10-26 20:23:17 +0200

    Finished

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from turtle import width
 from dash import Dash, callback, html, dcc, dash_table, Input, Output, State, MATCH, ALL, callback_context
 import dash_bootstrap_components as dbc
 import plotly.express as px
@@ -14,30 +15,29 @@ server = app.server
 
 sliders = [
 	dbc.Row([
-		dbc.Col(dbc.FormText('Heptane (€/l)', style={'color':'black'}), width=3),
-		dbc.Col(dcc.Input(id='heptane_input', value=.4341, style={'width':'100%'}), width=2),
-		dbc.Col(dcc.Slider(0, 2, .01, value=.4341, id='heptane', marks=None, tooltip={"placement": "bottom", "always_visible": True}),width=7)
+		dbc.Col(dbc.FormText('Heptane (€/unit)', style={'color':'black'}), width=3),
+		dbc.Col(dbc.FormText(id='heptane_unit', children=.4341, style={'width':'100%', 'color':'black'}), width=9),
 	]),
 	dbc.Row([
-		dbc.Col(dbc.FormText('Gas-oil (€/l)', style={'color':'black'}), width=3),
-		dbc.Col(dcc.Input(id='gas_input', value=1.281, style={'width':'100%'}), width=2),
-		dbc.Col(dcc.Slider(0, 2, .01, value=1.281, id='gas', marks=None, tooltip={"placement": "bottom", "always_visible": True}),width=7)
+		dbc.Col(dbc.FormText('Gas-oil (€/unit)', style={'color':'black'}), width=3),
+		dbc.Col(dbc.FormText(id='gas_unit', children=1.281, style={'width':'100%', 'color':'black'}), width=9),
 	]),
 	dbc.Row([
 		dbc.Col(dbc.FormText('Electricity (€/unit)', style={'color':'black'}), width=3),
-		dbc.Col(dcc.Input(id='electricity_input', value=.179, style={'width':'100%'}), width=2),
-		dbc.Col(dcc.Slider(0, 1, .01, value=.179, id='electricity', marks=None, tooltip={"placement": "bottom", "always_visible": True}),width=7)
+		dbc.Col(dbc.FormText(id='electricity_unit', children=.179, style={'width':'100%', 'color':'black'}), width=9),
 	]),
 	dbc.Row([
 		dbc.Col(dbc.FormText('Treatments + Staff (€/unit)', style={'color':'black'}), width=3),
-		dbc.Col(dcc.Input(id='treat_staff_input', value=2.90697, style={'width':'100%'}), width=2),
-		dbc.Col(dcc.Slider(0, 5, .01, value=2.90697, id='treat_staff', marks=None, tooltip={"placement": "bottom", "always_visible": True}),width=7)
+		dbc.Col(dcc.Input(id='treat_staff_input', value=2.8739, style={'width':'100%'}), width=2),
+		dbc.Col(dcc.Slider(0, 5, .01, value=2.8739, id='treat_staff', marks=None, tooltip={"placement": "bottom", "always_visible": True}),width=7)
 	]),
 	dbc.Row([
 		dbc.Col(dbc.FormText('Amortization (€/unit)*', style={'color':'black'}), width=3),
 		dbc.Col(dcc.Input(id='amort_input', value=.2, style={'width':'100%'}), width=2),
 		dbc.Col(dcc.Slider(0, 2, .01, value=.2, id='amort', marks=None, tooltip={"placement": "bottom", "always_visible": True}),width=7)
-	]),dbc.FormText('* The smaller the amortization, the longer the contract needs to go on in order to obtain ROI', style={'color':'black'})
+	]),
+	dbc.Row(dbc.FormText('* The smaller the amortization, the longer the contract needs to go on in order to obtain ROI', style={'color':'black'})),
+	dbc.Row(dbc.FormText(children='4 Years of amortization', id="years_amort", style={'color':'black', 'font-weight':'bold'})),
 ]
 
 
@@ -46,12 +46,29 @@ container = dbc.Container([
     dbc.Row([
 		html.H3('Remoin Ingeniería'),
 		], style={'text-align':'center', 'height': '100px'}),
-	dbc.Row(dcc.Graph(id="graph")),
+	dbc.Row([
+		dbc.Col(dcc.Graph(id="graph"), width=7),
+		dbc.Col([
+			dbc.Row(html.H5('Market price:')),
+			dbc.Row([
+				dbc.Col(dbc.FormText('Heptane (€/L)', style={'color':'black', 'width':'100%'}), width=4),
+				dbc.Col(dcc.Input(id='heptane_market', value=1.497), width=1)
+			]),
+			dbc.Row([
+				dbc.Col(dbc.FormText('Gas-oil (€/L)', style={'color':'black', 'width':'100%'}), width=4),
+				dbc.Col(dcc.Input(id='gas_market', value=1.25), width=1)
+			]),
+			dbc.Row([
+				dbc.Col(dbc.FormText('Electricity (€/kW)', style={'color':'black', 'width':'100%'}), width=4),
+				dbc.Col(dcc.Input(id='electricity_market', value=.25), width=1)
+			]),
+		], width=5, align="center"),
+	]),
 	html.Div(sliders),
     
 	], style={'margin-top':'20px', 'margin-bottom':'40px'})
 
-app.layout = html.Div([container])
+app.layout = container
 
 
 
@@ -60,13 +77,13 @@ app.layout = html.Div([container])
 # Actualiza la figura
 @callback(
     Output("graph", "figure"), 
-    Input("gas", "value"),
-	Input("heptane", "value"),
-	Input("electricity", "value"),
+	Output("years_amort", "children"), 
+    Input("gas_unit", "children"),
+	Input("heptane_unit", "children"),
+	Input("electricity_unit", "children"),
 	Input("treat_staff", "value"),
 	Input("amort", "value"))
 def update_bar_chart(gas, heptane, electricity, treat_staff, amort):
-
 
 	heptane_20 = 0.24
 	gas_20 = 0.72
@@ -77,14 +94,14 @@ def update_bar_chart(gas, heptane, electricity, treat_staff, amort):
 	earn_20 = 0.4464
 
 	heptane_22 = 0.4341
-	gas_22 = 1.281 # 1.34
-	stuff_22 = 2.8739 # 
+	gas_22 = 1.281
+	stuff_22 = 2.8739
 	amo_22 = 0.233
 	ele_22 = 0.179
 	price_22 = 5.501177
 	earn_22 = 0.500107 
 
-	costeSimulado = gas+heptane+electricity+treat_staff+amort
+	costeSimulado = float(gas)+float(heptane)+float(electricity)+treat_staff+amort
 
 	df = {
 		'year': ['2020']*3 + ['2022']*3 + ['Simulator']*3,
@@ -105,29 +122,51 @@ def update_bar_chart(gas, heptane, electricity, treat_staff, amort):
 	fig.update_layout(
 		yaxis_title="€ per unit",
 		legend_title_text='',
+		# title_pad_t=0,
 		title_text='Evolution of costs',
 		title_font_color="black",
 		title_x=0.5,
 		template='simple_white'
 		)
-	return fig
+	fig.update_layout(legend=dict(
+		orientation="h",
+		yanchor="bottom",
+		y=1.02,
+		xanchor="right",
+		x=1
+	))
+
+	fig.update_layout(
+		margin=dict(l=0, r=40, t=120, b=70),
+	)
+
+	return fig, f'{round(0.72*7/amort, 2)} Years of amortization'
 
 
 
 
 
 
-# Actualiza los sliders y los input
+# Actualiza las unidades por rueda en funcion del precio mercado
 @callback(
-    Output("gas", "value"),
-	Output("gas_input", "value"),
-	Input("gas", "value"),
-	Input("gas_input", "value"))
-def update_inputs(slider_value, input_value):
-	ctx = callback_context
-	trigger_id = ctx.triggered[0]["prop_id"].split(".")[0]
-	value = input_value if trigger_id == "gas_input" else slider_value
-	return float(value), float(value)
+    Output("heptane_unit", "children"),
+	Input("heptane_market", "value"))
+def update_heptane_units(heptane_market):
+	return heptane_market*0.29
+
+# Actualiza las unidades por rueda en funcion del precio mercado
+@callback(
+    Output("gas_unit", "children"),
+	Input("gas_market", "value"))
+def update_gas_units(gas_market):
+	return gas_market*1.34
+
+# Actualiza las unidades por rueda en funcion del precio mercado
+@callback(
+    Output("electricity_unit", "children"),
+	Input("electricity_market", "value"))
+def update_electricity_units(electricity_market):
+	return electricity_market*1.34
 
 # Actualiza los sliders y los input
 @callback(
